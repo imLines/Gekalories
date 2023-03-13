1a64e1a1605460866908467912600782b52a52b1
imLines <a.code.room@gmail.com>
2023-03-13 14:51:18 +0100

    Application capable of calculating caloric needs

---
+++ src/app.py
@@ -0,0 +1,87 @@
+import tkinter as tk
+from tkinter import ttk
+from kalories_requierement import KaloriesRequierement
+
+class GekaloriesApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Gekalories App')
+        self.geometry('400x900')
+        self.init_widget()
+
+
+    def init_widget(self):
+        vcmd = (self.register(self.validate_number_only))
+
+        title = tk.Label(self, text="Gegalories", font="Arial 24")
+        title.pack(pady=20)
+
+        frame_genre = tk.Frame(self, height=70, width=300)
+        label_genre = tk.Label(master=frame_genre, text="Tu es ")
+        self.select_genre = ttk.Combobox(frame_genre, values=['un homme', 'une femme'], state='readonly')
+        self.select_genre.current(0)
+        label_genre.pack()
+        self.select_genre.pack()
+        frame_genre.pack(pady=20)
+
+        frame_weight = tk.Frame(self, height=70, width=300)
+        label_weight = tk.Label(frame_weight, text="Poids (kg)")
+        self.input_weight = tk.Entry(frame_weight, validate='key', validatecommand=(vcmd, '%P'))
+        label_weight.pack()
+        self.input_weight.pack()
+        frame_weight.pack(pady=20)
+
+        frame_size = tk.Frame(self, height=70, width=300)
+        label_size = tk.Label(frame_size, text="Taille (cm)")
+        self.input_size = tk.Entry(frame_size, validate='key', validatecommand=(vcmd, '%P'))
+        label_size.pack()
+        self.input_size.pack()
+        frame_size.pack(pady=20)
+
+        frame_age = tk.Frame(self, height=70, width=300)
+        label_age = tk.Label(frame_age, text="Ton âge")
+        self.input_age = tk.Entry(frame_age, validate='key', validatecommand=(vcmd, '%P'))
+        label_age.pack()
+        self.input_age.pack()
+        frame_age.pack(pady=20)
+
+        frame_activity = tk.Frame(self, height=70, width=300)
+        label_activity = tk.Label(frame_activity, text="Quel est ton niveau d'activité ?")
+        self.select_activity = ttk.Combobox(frame_activity, values=['sédentaire', 'actif', 'modérément actif', 'extremement actif'], state='readonly')
+        self.select_activity.current(0)
+        label_activity.pack()
+        self.select_activity.pack()
+        frame_activity.pack(pady=20)
+
+        frame_objective = tk.Frame(self, height=70, width=300)
+        label_objective = tk.Label(frame_objective, text="Quel est ton objectif ?")
+        self.select_objective = ttk.Combobox(frame_objective, values=['perdre du poids', 'me maintenir', 'prendre du poids'], state='readonly')
+        self.select_objective.current(0)
+        label_objective.pack()
+        self.select_objective.pack()
+        frame_objective.pack(pady=20)
+
+        handle_submit = tk.Button(self, text="Calculer", command=self.submit).pack(pady=10)
+
+    def submit(self):
+        selected_genre = self.select_genre.get()
+        selected_weight = int(self.input_weight.get())
+        selected_size = int(self.input_size.get())
+        selected_age = int(self.input_age.get())
+        selected_activity = self.select_activity.get()
+        selected_objective = self.select_objective.get()
+        if selected_weight > 10 and selected_size > 100 and selected_age > 15:
+            new = KaloriesRequierement(self, selected_genre, selected_weight, selected_size, selected_age, selected_activity, selected_objective)
+
+
+
+    def validate_number_only(self, P):
+        if str.isdigit(P) or P == "":
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    app = GekaloriesApp()
+    app.mainloop()
---
+++ src/kalories_requierement.py
@@ -0,0 +1,76 @@
+import tkinter as tk
+
+
+class KaloriesRequierement(tk.Toplevel):
+    def __init__(self, parent, genre, weight, size, age, activity, objective):
+        super().__init__(parent)
+        self.title('Gekalories - Mon besoin calorique')
+        self.geometry('600x300')
+        self.genre = genre
+        self.weight = weight
+        self.size = size
+        self.age = age
+        self.activity = activity
+        self.objective = objective
+        self.init_widget()
+
+    def init_widget(self):
+        frame_init = tk.Frame(self, height=50, width=300)
+        label_genre = tk.Label(frame_init, text=f'Genre : {self.genre}').pack()
+        label_weight = tk.Label(frame_init, text=f'Poids : {self.weight}Kg').pack()
+        label_size = tk.Label(frame_init, text=f'Taille : {self.size}cm').pack()
+        label_age = tk.Label(frame_init, text=f'Age : {self.age}ans').pack()
+        label_activity = tk.Label(frame_init, text=f'Activité : {self.activity}').pack()
+        label_objective = tk.Label(frame_init, text=f'Objectif : {self.objective}').pack()
+        frame_init.pack(pady=20)
+
+        self.bmr = self.calcul_bmr()
+        self.ttde = self.calcul_ttde()
+        self.final_cal = self.calcul_final_cal()
+
+        frame_info = tk.Frame(self, height=10, width=300)
+        label_bmr = tk.Label(frame_info, text=f'Ton corps à besoin de {self.bmr}kcal pour fonctionner.').pack()
+        label_ttde = tk.Label(frame_info, text=f"Ton niveau d'activité est {self.activity}, ton besoin calorique est {self.ttde}kcal").pack()
+        label_final_cal = tk.Label(frame_info, text=f"Pour parvenir à tes objectifs, tu dois consommer {self.final_cal}kcal.")
+        frame_info.pack()
+
+    def calcul_ttde(self):
+        if self.activity == 'sédentaire':
+            ttde = self.bmr*1.55
+            return int(ttde)
+        elif self.activity == 'actif':
+            ttde = self.bmr * 1.85
+            return int(ttde)
+        elif self.activity == 'modérément actif':
+            ttde = self.bmr * 2.2
+            return int(ttde)
+        elif self.activity == 'extremement actif':
+            ttde = self.bmr * 2.4
+            return int(ttde)
+
+
+    def calcul_bmr(self):
+        if self.genre == 'un homme':
+            first = 10 * self.weight
+            second = 6.25 * self.size
+            third = 5 * self.age
+            bmr = first+second-third+5
+            return int(bmr)
+        elif self.genre == 'une femme':
+            first = 10 * self.weight
+            second = 6.25 * self.size
+            third = 5 * self.age
+            bmr = first+second-third-161
+            return int(bmr)
+
+    def calcul_final_cal(self):
+        if self.objective == 'perdre du poids':
+            final_cal = self.ttde-500
+            return final_cal
+        elif self.objective == 'me maintenir':
+            final_cal = self.ttde
+            return final_cal
+        elif self.objective == 'prendre du poids':
+            final_cal = self.ttde + 500
+            return  final_cal
+
